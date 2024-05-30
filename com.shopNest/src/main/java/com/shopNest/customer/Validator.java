c115d9b01f57240f1ea2546dc5dbf51e5c828962
TABARAK MUKHTAR <107870133+tabarakmukhtar@users.noreply.github.com>
2024-05-30 10:58:06 +0530

    commit

---
+++ com.shopNest/src/main/java/com/shopNest/customer/Validator.java
@@ -0,0 +1,17 @@
+package com.shopNest.customer;
+
+import com.shopNest.dbHandler.DataFetcher;
+
+public class Validator {
+
+	public static boolean isValid(String  uname, String  pass) {
+		 String dbPass=DataFetcher.fetchPassword(uname);
+		 if(pass.equals(dbPass)) {
+		return true;
+	}
+		 else {
+			 return false;
+		 }
+	}
+
+}
